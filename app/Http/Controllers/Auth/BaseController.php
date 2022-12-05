6afa10b02555ac248d9afb98d481ef22a2331e24
Leo <leonardmartirosyan95@gmail.com>
2022-12-06 00:38:29 +0400

    Fix Auth Seperate

--- app/Http/Controllers/Auth/BaseController.php
+++ app/Http/Controllers/Auth/BaseController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
 
 
+use App\Http\Controllers\Controller;
 use App\Services\Auth\Services;
 
 class BaseController extends Controller
